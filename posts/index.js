2d2e862b0891a65360a9337dfb501f3f473f2a14
hawrot <mhawro200@caledonian.ac.uk>
2020-06-13 21:25:01 +0100

    Add post

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
@@ -11,6 +13,12 @@ app.get('/posts', (req, res) => {
 
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    post[id] = {id, title};
+
+    res.status(201).send(posts[id]);
+
 });
 
 app.listen(4000, () => {
