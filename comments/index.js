a26a29a68e585b7e6e1d7a7f5ff3ac8987b49bff
hawrot <mhawro200@caledonian.ac.uk>
2020-06-15 00:16:50 +0100

    Updating comment content

--- comments/index.js
+++ comments/index.js
@@ -37,8 +37,32 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received events', req.body.type);
+
+    const {type, data} = req.body;
+
+    if(type === 'CommentModerated'){
+        const {postId, id, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status;
+
+        await axios.post('http://localhost:4005', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        })
+    }
+
     res.send({});
 })
 
