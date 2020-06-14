e77289c8cacb1472a3882f88e634b879e4f963df
hawrot <mhawro200@caledonian.ac.uk>
2020-06-15 00:02:19 +0100

    Add status for the comment creation

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: 'pending'});
 
     commentsByPostId[req.params.id] = comments;
 
