76867c1dd26c7ead7b4fd704a224627b0bc0ed61
hawrot <mhawro200@caledonian.ac.uk>
2020-06-15 23:36:41 +0100

    Adjust paths to the kubernetes services

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,17 +13,15 @@ app.post('/events', (req, res) => {
     events.push(event);
     //4000 is a posts service
     axios.post('http://posts-clusterip-srv:4000/events', event);
-/*
 
     //4001 is a comments service
-    axios.post('http://localhost:4001/events', event);
+    axios.post('http://comments-srv:4001/events', event);
 
     //4002 is a query service
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://query-srv:4002/events', event);
 
     //4003 is a moderation service
-    axios.post('http://localhost:4003/events', event);
-*/
+    axios.post('http://moderation-srv:4003/events', event);
 
 
 
